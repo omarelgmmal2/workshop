5bbc9c3f0fcd8630f2c61d1763dacaa88f19ebc1
OmarElgmmal <omarelgmmal2>
2025-03-12 19:02:57 +0200

    complete cart page

--- lib/view/pages/cart/cart_page.dart
+++ lib/view/pages/cart/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:workshop_app/core/design/app_button.dart';
 import 'package:workshop_app/view/widget/custom_cart_item.dart';
 import '../../../core/design/custom_app_bar.dart';
 import '../../../core/design/title_text.dart';
@@ -15,6 +16,57 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff0AA7CB),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.only(top: 20.h,right: 20.w,left: 20.w),
+        height: 150.h,
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: AppColor.black,
+              width: 2,
+            ),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              children: [
+                CustomTextWidget(
+                    label: "Sub Total",
+                  style: TextStyleTheme.textStyle15Bold.copyWith(
+                    color: AppColor.black,
+                  ),
+                ),
+                Spacer(),
+                CustomTextWidget(
+                  label: "\$200.00",
+                  style: TextStyleTheme.textStyle15Bold.copyWith(
+                    color: AppColor.black,
+                  ),
+                ),
+              ],
+            ),
+            verticalSpace(5),
+            CustomTextWidget(
+              label: "(Total dosn’t include shipping)",
+              style: TextStyleTheme.textStyle15Bold.copyWith(
+                color: AppColor.black,
+              ),
+            ),
+            verticalSpace(16),
+            AppButton(
+                text: "Check out",
+                textStyle: TextStyleTheme.textStyle18SemiBold,
+                buttonStyle: ElevatedButton.styleFrom(
+                  backgroundColor: AppColor.primary,
+                  //backgroundColor: Color(0xff0AA7CB),
+                ),
+                onPress: () {},
+            ),
+          ],
+        ),
+      ),
       appBar: CustomAppBar(
         height: 50.h,
         padding: EdgeInsets.symmetric(horizontal: 16.w),
